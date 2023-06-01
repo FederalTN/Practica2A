6a391be28105e1bdac7c9c3d2c05f9c5b2cdbd32
Federal <espada_2255@hotmail.com>
2023-05-31 21:02:46 -0700

    Categorias/Productos asignados

--- HTTP/app.py
+++ HTTP/app.py
@@ -1,63 +1,76 @@
-from flask import Flask, request, jsonify,json
+from flask import Flask, request, jsonify
 import requests
+from itertools import cycle
 from dotenv import load_dotenv
 import os
+import json
 
 app = Flask(__name__)
 load_dotenv()
 
 @app.route('/')
 def hello():
-    return 'Hola mundo!, buscador de retail distribuido a su disposiciòn!'
+    return '¡Hola mundo! ¡Buscador de retail distribuido a su disposición!'
 
 # Especificar los endpoints de los esclavos
 esclavos = os.getenv("ESCLAVOS").split(',')
+categorias_esclavos = {esclavo: [] for esclavo in esclavos}
+productos_esclavos = {esclavo: [] for esclavo in esclavos}
 
-# El índice del esclavo que se usará para la próxima búsqueda
-index_esclavo = 0
+# Obtener la lista de categorías y productos del archivo JSON
+with open('./database.json') as f:
+    data = json.load(f)
+    products = data['products']
+    categorias = set([product['categoria'] for product in products])
+    pnames = set([product['pname'] for product in products])
+
+# Asignar categorías y productos a los esclavos mediante el algoritmo round-robin
+esclavo_cycle = cycle(esclavos)
+for categoria in categorias:
+    esclavo = next(esclavo_cycle)
+    categorias_esclavos[esclavo].append(categoria)
+
+
+esclavo_cycle = cycle(esclavos)
+for product in pnames:
+    esclavo = next(esclavo_cycle)
+    productos_esclavos[esclavo].append(product)
 
 # La ruta "/query" activará la función getProductsQuery()
-@app.route('/query', methods=['GET']) 
+@app.route('/query', methods=['GET'])
 def getProductsQuery():
-    global index_esclavo  # Declarar la variable global
-
-    # Verifica si se esta realizando una busqueda por categoria o categoria
+    # Verificar si se está realizando una búsqueda por categorías o productos
     if 'categorias' in request.args or 'productos' in request.args:
-        # Si "categorias" o "productos" estan en los argumentos, se realizara la busqueda correspondiente
+        # Obtener las categorías o productos de los argumentos de la solicitud
         if 'categorias' in request.args:
             consultas = request.args['categorias']
             argDeseado = 'categorias'
+            esclavos_consulta = categorias_esclavos
         else:
             consultas = request.args['productos']
             argDeseado = 'productos'
+            esclavos_consulta = productos_esclavos
 
-        # Se convierte la consulta en una lista
+        # Dividir las consultas en una lista
         consultas_list = consultas.split()
-
-        # Distribuir los productos entre los esclavos usando el algoritmo round-robin
-        resultados_esclavos = {esclavo: [] for esclavo in esclavos}
-        for i, consulta in enumerate(consultas_list):
-            # Obtener el esclavo que se usará para esta búsqueda
-            esclavo = esclavos[index_esclavo]
-            index_esclavo = (index_esclavo + 1) % len(esclavos)
-
-            # Añadir el producto o categoria a la lista de busqueda en el esclavo correspondiente
-            resultados_esclavos[esclavo].append(consulta)
-
-        # Reset del indice (para mantener orden de las proximas busquedas)
-        index_esclavo = 0
-
-        # Realizar el query en cada esclavo de acuerdo a su lista de busqueda
+        
+        # Realizar la búsqueda en los esclavos correspondientes
         results = []
-        for esclavo in esclavos:
-            if len(resultados_esclavos[esclavo]) > 0:
-                # Hacer la búsqueda en el esclavo correspondiente
-                respuesta = requests.get(f'{esclavo}/query', params={argDeseado: ' '.join(resultados_esclavos[esclavo])})
+        for esclavo, consultas_esclavo in esclavos_consulta.items():
+            # Filtrar las consultas del esclavo que coinciden con las consultas de la solicitud
+            consultas_filtradas = [consulta for consulta in consultas_list if consulta in consultas_esclavo]
+
+            if consultas_filtradas:
+                # Realizar la búsqueda en el esclavo correspondiente
+                url = f'{esclavo}/query'
+                params = {argDeseado: ' '.join(consultas_filtradas)}
+                respuesta = requests.get(url, params=params)
+
                 if respuesta.status_code == 200:
-                    results += respuesta.json()['results']
+                    results += respuesta.json().get('results', [])
 
         # Devolver los resultados
-        return {"results": results}
+        return jsonify({"results": results})
+
     else:
         return "No se encontraron productos."
-    
