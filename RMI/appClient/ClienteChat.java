2cad485cf59bef1781074cfb65229dc17a7a6f56
Federal <espada_2255@hotmail.com>
2023-06-01 19:27:41 -0700

    ClienteRMI leyendo logs de LOGS

--- RMI/appClient/ClienteChat.java
+++ RMI/appClient/ClienteChat.java
@@ -3,6 +3,9 @@ import java.rmi.registry.Registry;
 import java.rmi.RemoteException;
 import java.util.*;
 import java.text.SimpleDateFormat;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 class ClienteChat {
 
@@ -23,39 +26,64 @@ class ClienteChat {
 
             srv.alta(c);
 
-            Scanner ent = new Scanner(System.in);
-
             String apodo = args[0];
-            System.out.print(apodo + "! te doy la bienvenida al chat!" + "\n" + "Escriba algo -> ");
+            System.out.print("Inicializado, cliente: " + apodo + "\n");
             String msg;
             String msgExit = "EXIT";
-            // Input del chat
-            while (ent.hasNextLine()) {
-                msg = ent.nextLine();
-                // Desconexión del cliente
-                if (msg.equals(msgExit)) {
-                    break;
-                } else {
-                    // Registro correlativo
-                    int numeroCorrelativo = srv.numeroCorrelativoActual();
-                    // Registro fecha acciòn
-                    Date fecha = new Date();
-                    SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd;HH:mm:ss");
-                    String fechaFormateada = formatoFecha.format(fecha);
-                    String MensajeRegistro = numeroCorrelativo + ";" + fechaFormateada + ";" + apodo + ": " + msg;
-                    // Envio de mensaje
-                    srv.envio(c, apodo, MensajeRegistro);
-                    System.out.println(MensajeRegistro);
-                }
-            }
+            
+            // Ruta al archivo .log
+            String archivoLog = "../../LOGS/log_1.log";  
+            // 5 segundos en milisegundos
+            int tiempoEspera = 5000;  
+            leerArchivoLog(srv, c, apodo, archivoLog, tiempoEspera);
+
             // Avisa desconexión del cliente al servidor
             srv.baja(c);
             System.exit(0);
         } catch (RemoteException e) {
             System.err.println("Error de comunicacion: " + e.toString());
         } catch (Exception e) {
-            System.err.println("Excepcion en ClienteChat:");
+            System.err.println("Excepcion en Cliente:");
             e.printStackTrace();
+        }
+    }
+
+    private static void leerArchivoLog(ServicioChat srv, ClienteImpl cliente, String nombreCliente, String archivoLog, int tiempoEspera) throws RemoteException {
+        long ultimaLineaLeida = 0;  // Variable para almacenar el número de la última línea leída
+
+        while (true) {
+            try (BufferedReader br = new BufferedReader(new FileReader(archivoLog))) {
+                // Saltar las líneas previamente leídas
+                for (long i = 0; i < ultimaLineaLeida; i++) {
+                    br.readLine();
+                }
+
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    // Registro correlativo
+                    int numeroCorrelativo = srv.numeroCorrelativoActual();
+                    // Registro fecha acción
+                    Date fecha = new Date();
+                    SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd;HH:mm:ss");
+                    String fechaFormateada = formatoFecha.format(fecha);
+                    String mensajeRegistro = linea + ";" + fechaFormateada + "; cliente" + nombreCliente;
+                    // Envío de mensaje
+                    srv.envio(cliente, nombreCliente, mensajeRegistro);
+                    System.out.println(mensajeRegistro);
+
+                    ultimaLineaLeida++;  // Actualizar el número de la última línea leída
+                }
+            } catch (IOException e) {
+                System.err.println("Error al leer el archivo " + archivoLog);
+                e.printStackTrace();
+            }
+
+            try {
+                Thread.sleep(tiempoEspera);  // Esperar el tiempo especificado antes de la próxima lectura
+            } catch (InterruptedException e) {
+                System.err.println("Error al esperar el tiempo especificado.");
+                e.printStackTrace();
+            }
         }
     }
 }
