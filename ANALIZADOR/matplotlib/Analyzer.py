80c6fe9246a7e585361512f512861c0140544509
Federal <espada_2255@hotmail.com>
2023-07-09 11:46:18 -0700

    Finalizado

--- ANALIZADOR/matplotlib/Analyzer.py
+++ ANALIZADOR/matplotlib/Analyzer.py
@@ -3,52 +3,60 @@ import re
 
 archivo = '../../RMI/appServer/logCentralizado.txt'
 
-pruebas = []
-indice = 0
+pruebas_categorias = []
+pruebas_productos = []
+indice_categorias = 0
+indice_productos = 0
 conteo = 0
+
 with open(archivo, 'r') as f:
     for linea in f:
-        # Ignorar líneas de inicio de conexión de cliente
         if not linea.startswith('inicio de conexion'):
             conteo += 1
             if (conteo == 1):
-                # Buscar los timestamps en la línea
                 timestamps = re.findall(r'; (\d+)', linea)
-                
-                # Extraer el primer timestamp como entero
                 primer_timestamp = int(timestamps[0])
             elif (conteo == 6):
-                # Buscar los timestamps en la línea
                 timestamps = re.findall(r'; (\d+)', linea)
-
-                # Extraer el ultimo timestamp como entero
                 ultimo_timestamp = int(timestamps[-1])
 
-                # Se añaden al arreglo
-                indice += 1
-                pruebas.append((indice, ultimo_timestamp - primer_timestamp))
-
                 conteo = 0
-                # Hacer lo que necesites con los timestamps extraídos
-                print('timestamp:', pruebas)
-                print('---')
-            
-# ../../RMI/appServer/logCentralizado.txt
 
-# Separa los valores de x (ID de pruebas) y y (tiempo en milisegundos)
-x = [tupla[0] for tupla in pruebas]
-y = [tupla[1] for tupla in pruebas]
+                if 'buscar por categorias' in linea:
+                    indice_categorias += 1
+                    pruebas_categorias.append((indice_categorias, ultimo_timestamp - primer_timestamp))
+                elif 'buscar por productos' in linea:
+                    indice_productos += 1
+                    pruebas_productos.append((indice_productos, ultimo_timestamp - primer_timestamp))
 
-# Crea el gráfico con los puntos juntos
-plt.plot(x, y, '-o')
+print('Análisis de "buscar por categorías"')
+print('timestamp:', pruebas_categorias)
+print('---')
 
-# Agrega etiquetas y título
-plt.xlabel('ID de pruebas')
+print('Análisis de "buscar por productos"')
+print('timestamp:', pruebas_productos)
+print('---')
+
+# Graficar resultados para "buscar por categorías"
+x_categorias = [tupla[0] for tupla in pruebas_categorias]
+y_categorias = [tupla[1] for tupla in pruebas_categorias]
+
+plt.plot(x_categorias, y_categorias, '-o')
+plt.xlabel('Pruebas')
 plt.ylabel('Tiempo (ms)')
-plt.title('Gráfico de tiempo de pruebas')
+plt.title('Gráfico de tiempo de pruebas - Buscar por categorías')
+plt.text(0, 0.95, 'tamaños de database [100,500,1000,10000,100000,1000000,1500000]', transform=plt.gca().transAxes, fontsize=10, va='top')
+plt.savefig('grafico_categorias.png')
+plt.show()
 
-# Se guarda la imagen
-plt.savefig('grafico.png')
+# Graficar resultados para "buscar por productos"
+x_productos = [tupla[0] for tupla in pruebas_productos]
+y_productos = [tupla[1] for tupla in pruebas_productos]
 
-# Muestra el gráfico
+plt.plot(x_productos, y_productos, '-o')
+plt.xlabel('Pruebas')
+plt.ylabel('Tiempo (ms)')
+plt.title('Gráfico de tiempo de pruebas - Buscar por productos')
+plt.text(0, 0.95, 'tamaños de database [100,500,1000,10000,100000,1000000,1500000]', transform=plt.gca().transAxes, fontsize=10, va='top')
+plt.savefig('grafico_productos.png')
 plt.show()
